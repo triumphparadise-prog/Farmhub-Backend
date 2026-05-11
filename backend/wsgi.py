eb9e70118845fe2e852567f142a7b97edabe9d15
PREYE <akordonald.15@gmail.com>
2026-05-11 15:39:56 +0100

    Run migrations on production startup

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -9,8 +9,23 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 
+from django.core.management import call_command
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings.prod')
 
 application = get_wsgi_application()
+
+
+def run_startup_migrations():
+    if os.environ.get("ENV") != "production":
+        return
+
+    enabled = os.environ.get("RUN_MIGRATIONS_ON_STARTUP", "true").lower()
+    if enabled not in {"1", "true", "yes"}:
+        return
+
+    call_command("migrate", interactive=False, verbosity=1)
+
+
+run_startup_migrations()
